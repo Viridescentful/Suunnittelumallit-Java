4bcc3da679dc59aad3700d1bd3d65a1a0cf56693
veikk <valttejo@metropolia.fi>
2025-09-25 13:06:46 +0300

    Add calendar adapter implementation with new date interface

--- src/Calander_Adapter/Main.java
+++ src/Calander_Adapter/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         NewDateInterface newDate = new CalendarToNewDateAdapter();
 
-        newDate.setYear(2025);
-        newDate.setMonth(9);
-        newDate.setDay(25);
+        newDate.setYear(2029);
+        newDate.setMonth(2);
+        newDate.setDay(12);
 
         System.out.println("Date: " + newDate);
 
-        newDate.advanceDays(10);
+        newDate.advanceDays(120);
 
         System.out.println("Date: " + newDate);
     }
